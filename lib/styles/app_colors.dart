c653e72aa4d63d64754a5d064d822204f1bfba4d
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-05-15 18:06:04 +0100

    chore: add app colors

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const dark = Color(0xff202123);
+  static const middle = Color(0xff343541);
+  static const bright = Color(0xff444654);
+
+  static const darkGreen = Color(0xff00897B);
+  static const lightGreen = Color(0xff8DCDB8);
+  static const accentGreen = Color(0xff0FA47F);
+
+  static const greyFootprint = Color(0xFF9A9B9F);
+  static const greyIcons = Color(0xffC5C5D1);
+  static const brightGrey = Color(0xffECECF1);
+
+  static const white = Color(0xffFFFFFF);
+
+  static const blue = Color(0xff5536DA);
+}
